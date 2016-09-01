13ebe9686c1559072f068509be26f3a32ad7192c
dmnpignaud <dmnpignaud@gmail.com>
2016-09-01 18:01:47 +0200

    changed home endpoint into /documentation
    use express 4.0.0

--- app.js
+++ app.js
@@ -19,24 +19,21 @@ glob('**/*.avsc', {cwd: schema_dir}, function (err, files) {
 // Precompile dust templates at app startup, and then serve them out of memory
 var dust_templates = content.dustTemplates();
 
+var router  = express.Router();
 var app = express();
 
-app.configure(function () {
-    app.set('port', process.env.PORT || 8124);
-    app.use(express.logger('dev'));
-    app.use(express.bodyParser());
-    app.use(express.methodOverride());
-    app.use(require('less-middleware')({ src: __dirname + '/public' }));
-    app.use(express.static(path.join(__dirname, 'public')));
-});
+app.set('port', process.env.PORT || 8124);
+app.use(require('less-middleware')({ src: __dirname + '/public' }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function (req, res) {
+
+router.get('/', function (req, res) {
     content.topLevelHTML({schemata: schemata}, function (err, html) {
         res.set('Content-Type', 'text/html').send(html);
     });
 });
 
-app.get('/dust-templates.js', function (req, res) {
+router.get('/dust-templates.js', function (req, res) {
     res.set('Content-Type', 'text/javascript').send(dust_templates);
 });
 
@@ -49,6 +46,8 @@ app.get(/^\/schemata\/(\w[\w.\-]*(?:\/\w[\w.\-]*)*)$/, function (req, res) {
         }
     });
 });
+
+app.use('/documentation', router);
 
 http.createServer(app).listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
